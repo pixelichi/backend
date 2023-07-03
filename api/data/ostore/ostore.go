9188f6506b2bc3b26e7b316236609d3df83d67fe
Pixelichi <pixelichi@gmail.com>
2023-07-02 20:48:34 -0700

    oops mak more updated in this

--- api/data/ostore/ostore.go
+++ api/data/ostore/ostore.go
@@ -14,8 +14,7 @@ const (
 
 // OStore is an interface for integrating with s3 compliant object stores
 type OStore interface {
-  createBucketIfDoesntExist(c *gin.Context, bucketName string)
-  UploadFile(c *gin.Context, bucketName, objectName string, reader io.Reader, fileSize int64) error
-  GetSignedUrlForUserDataFile(c *gin.Context, bucket string, object string, expDuration time.Duration, reqParams url.Values) (url.URL, error)
-  GetSignedUrlForUserDataFileEmptyParam(c *gin.Context, bucket string, object string, expDuration time.Duration) (url.URL, error)
+	createBucketIfDoesntExist(c *gin.Context, bucketName string)
+  UploadObject(c *gin.Context, bucketName, objectName string, reader io.Reader, fileSize int64) error
+	GetSignedUrlForObject(c *gin.Context, bucket string, object string, expDuration time.Duration, reqParams url.Values) (url.URL, error)
 }
--- api/data/ostore_txn/user_data.go
+++ api/data/ostore_txn/user_data.go
@@ -12,30 +12,26 @@ import (
 	"shinypothos.com/util/image_util"
 )
 
-const UserDataBucket string = "shinypothos-user-data"
-
-// func GetProfilePicUrl(c context.Context, s *common.Server) (string , error){
-//   (*s.ObjectStore).GetSignedUrlForUserDataFile(c,)
-// }
+const (
+	UserDataBucket string = "shinypothos-user-data"
+)
 
 func getUserDataObjectPrefix(userID int64) string {
 	return strconv.FormatInt(userID, 10) + "/"
 }
 
-func UploadFileToUserDataOrAbort(c *gin.Context, os *ostore.OStore, userId int64, image image_util.Image, objectName string) {
-	err := (*os).UploadFile(c, UserDataBucket, getUserDataObjectPrefix(userId)+objectName, image.Reader, int64(image.Size))
-	
-  if err != nil {
+func uploadFileToUserDataOrAbort(c *gin.Context, os *ostore.OStore, userId int64, image image_util.Image, objectName string) {
+	err := (*os).UploadObject(c, UserDataBucket, getUserDataObjectPrefix(userId)+objectName, image.Reader, int64(image.Size))
+
+	if err != nil {
 		c.AbortWithStatusJSON(http.StatusInternalServerError, server_error.NewInternalServerError("Unable to upload file."))
 	}
 }
 
-
-func GetSignedUrlForUserDataFileOrAbort(c *gin.Context, os *ostore.OStore, object string, expDuration time.Duration, ) (url.URL) {
-
-	presignedURL, err := (*os).GetSignedUrlForUserDataFileEmptyParam(c, UserDataBucket, "myobject", expDuration)
+func getPreSignedUrlForUserDataFileOrAbort(c *gin.Context, os *ostore.OStore, userId int64, object string, expDuration time.Duration) url.URL {
+	presignedURL, err := (*os).GetSignedUrlForObject(c, UserDataBucket, strconv.FormatInt(userId, 10) + "/" + object, expDuration, url.Values{})
 	if err != nil {
-    c.AbortWithStatusJSON(http.StatusInternalServerError, server_error.NewInternalServerError("Could not get pre-signed url for file."))
+		c.AbortWithStatusJSON(http.StatusInternalServerError, server_error.NewInternalServerError("Could not get pre-signed url for file."))
 	}
 
 	return presignedURL
